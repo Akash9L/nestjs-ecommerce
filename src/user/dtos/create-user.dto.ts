daf9218c9f5a4d63e394be350219160351bd8925
Akash Navlakha <akashnavlakha024@gmail.com>
2023-01-06 12:00:33 +0530

    User Module

---
+++ src/user/dtos/create-user.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Role } from 'src/auth/enums/role.enum';
+
+export class CreateUserDTO {
+  @ApiProperty()
+  username: string;
+  @ApiProperty()
+  email: string;
+  @ApiProperty()
+  password: string;
+  @ApiProperty()
+  roles: Role[];
+}
